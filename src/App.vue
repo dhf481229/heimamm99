f6ee820caf8932da7d8c9665ef7fdfa528207cf0
dhf <you@example.com>
2020-04-05 10:06:02 +0800

    @使用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <tt></tt>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+import tt from "@/components/tt.vue";
+// 导入子组件
+// import 组件名 from  "路径"
+// @就是代表了src路径
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    tt
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* 如何导入刚才的css 
+注意：@在style中的使用需要加一个~@
+*/
+@import url(~@/assets/base.css);
 </style>
---
+++ src/components/tt.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+<script>
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "./HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
+<style>
+</style>
