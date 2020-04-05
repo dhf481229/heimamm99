355cbff1421029b670be2dd7f2ea50e407251dce
dhf <you@example.com>
2020-04-05 10:20:43 +0800

    项目起步 - 删除默认代码

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="@/assets/logo.png" />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <tt></tt>
-  </div>
-</template>
+<template></template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import HelloWorld from "@/components/HelloWorld.vue";
-import tt from "@/components/tt.vue";
-// 导入子组件
-// import 组件名 from  "路径"
-// @就是代表了src路径
-export default {
-  name: "App",
-  components: {
-    tt
-  }
-};
+export default {};
 </script>
 
 <style>
-/* 如何导入刚才的css 
-注意：@在style中的使用需要加一个~@
-*/
-@import url(~@/assets/base.css);
 </style>
