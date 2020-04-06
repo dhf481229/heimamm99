2c85ff3bbea3f5deba20b15bb2424a84e99a7836
dhf <you@example.com>
2020-04-06 17:11:08 +0800

    注册模块-标题栏样式处理

--- src/view/login/register.vue
+++ src/view/login/register.vue
@@ -1,9 +1,13 @@
 <template>
-  <el-dialog :visible.sync="dialogFormVisible">
+  <el-dialog :visible.sync="dialogFormVisible" class="register" width="600px" :show-close="false">
     <!-- 
       el-dialog
       visible.sync属性控该弹窗口是否显示
+      slot可以重写title
+      width属性用于设置宽度
+      show-close	是否显示关闭按钮
     -->
+    <div slot="title" class="title">用户注册</div>
   </el-dialog>
 </template>
 <script>
@@ -15,5 +19,17 @@ export default {
   }
 };
 </script>
-<style>
+<style lang="less">
+.register {
+  .title {
+    height: 53px;
+    background: rgba(3, 192, 249, 1);
+    color: #fff;
+    text-align: center;
+    line-height: 53px;
+  }
+  .el-dialog__header {
+    padding: 0;
+  }
+}
 </style>
